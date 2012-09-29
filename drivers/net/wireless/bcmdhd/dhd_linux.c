b9a721a89ba9ef0810aad8dcd3bd95634d4cae2c
Simone Renzo <simone201@gmail.com>
2012-09-29 13:19:39 +0200

    bcmdhd: Change packet filter settings to block multicast
    A common complaint of users is that battery life on wifi
    is very poor, with frequent wlan_rx_wake wakelocks.
    
    It turns out Samsung "opened up" the packet filter
    settings from Gingerbread to ICS.
    
    This makes them match the tuna configuration, minus
    the filter to allow mDNS through (which lets a bunch
    of other stuff through too.)
    
    The original patch reverted to Gingerbread settings,
    but it seems like the filter definitions changed
    from GB to ICS.
    
    Change-Id: Ib56d50374d86733f9a2893b5a4afbcc6cd859a75
    Signed-off-by: Andrew Dodd <atd7@cornell.edu>

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -3913,30 +3913,8 @@ dhd_preinit_ioctls(dhd_pub_t *dhd)
 	/* Add filter to pass multicastDNS packet and NOT filter out as Broadcast */
 	dhd->pktfilter[4] = "104 0 0 0 0xFFFFFFFFFFFF 0x01005E0000FB";
 
-#ifdef CUSTOMER_HW4
-#ifdef GAN_LITE_NAT_KEEPALIVE_FILTER
-	dhd->pktfilter_count = 4;
-	/* Setup filter to block broadcast and NAT Keepalive packets */
-	dhd->pktfilter[0] = "100 0 0 0 0xffffff 0xffffff"; /* discard all broadcast packets */
-	dhd->pktfilter[1] = "102 0 0 36 0xffffffff 0x11940009"; /* discard NAT Keepalive packets */
-	dhd->pktfilter[2] = "104 0 0 38 0xffffffff 0x11940009"; /* discard NAT Keepalive packets */
-	dhd->pktfilter[3] = NULL;
-#else
-	/* Setup filter to allow only unicast */
-#if defined(BLOCK_IPV6_PACKET)
-	dhd->pktfilter[0] = "100 0 0 0 "
-		HEX_PREF_STR UNI_FILTER_STR ZERO_ADDR_STR ETHER_TYPE_STR IPV6_FILTER_STR
-		" "
-		HEX_PREF_STR ZERO_ADDR_STR ZERO_ADDR_STR ETHER_TYPE_STR ZERO_TYPE_STR;
-#else
-	dhd->pktfilter[0] = "100 0 0 0 0x01 0x00";
-#endif /* BLOCK_IPV6_PACKET */
-#if defined(PASS_IPV4_SUSPEND)
-	dhd->pktfilter_count = 5;
-	dhd->pktfilter[4] = "104 0 0 0 0xFFFFFF 0x01005E";
-#endif
-#endif /* GAN_LITE_NAT_KEEPALIVE_FILTER */
-#endif /* CUSTOMER_HW4 */
+	dhd->pktfilter_count = 1;
+	/* Setup filter to allow unicast only */
 
 #if defined(SOFTAP)
 	if (ap_fw_loaded) {
