a95773dbd53b94288070837711ab455ea78a7a94
Simone Renzo <simone201@gmail.com>
2012-10-16 16:36:33 +0200

    stand_hotplug: ported NEAK S3 features

--- arch/arm/mach-exynos/stand-hotplug.c
+++ arch/arm/mach-exynos/stand-hotplug.c
@@ -28,6 +28,7 @@
 #include <linux/reboot.h>
 #include <linux/gpio.h>
 #include <linux/cpufreq.h>
+#include <linux/earlysuspend.h>
 
 #include <plat/map-base.h>
 #include <plat/gpio-cfg.h>
@@ -36,6 +37,10 @@
 
 #include <mach/regs-gpio.h>
 #include <mach/regs-irq.h>
+
+/* simone201-tegrak multicore includes */
+#include <linux/device.h>
+#include <linux/miscdevice.h>
 
 #if defined(CONFIG_MACH_P10)
 #define TRANS_LOAD_H0 5
@@ -146,6 +151,9 @@ struct cpu_hotplug_info {
 
 static DEFINE_PER_CPU(struct cpu_time_info, hotplug_cpu_time);
 
+static bool screen_off;
+static bool standhotplug_enabled = true;
+
 /* mutex can be used since hotplug_timer does not run in
    timer(softirq) context but in process context */
 static DEFINE_MUTEX(hotplug_lock);
@@ -163,6 +171,12 @@ bool hotplug_out_chk(unsigned int nr_online_cpu, unsigned int threshold_up,
 		cur_freq <= freq_min));
 #endif
 }
+
+/* simone201-tegrak multi-core values */
+#define MULTI_CORE_VERSION 1
+#define NR_OF_CORES 4
+int cores_on = NR_OF_CORES;
+int hotplug_on = 1;
 
 static inline enum flag
 standalone_hotplug(unsigned int load, unsigned long nr_rq_min, unsigned int cpu_rq_min)
@@ -233,6 +247,33 @@ static void hotplug_timer(struct work_struct *work)
 
 	mutex_lock(&hotplug_lock);
 
+	if (!standhotplug_enabled) {
+		printk(KERN_INFO "pm-hotplug: disable cpu auto-hotplug\n");
+		goto off_hotplug;
+	}
+
+	if (screen_off && !cpu_online(1) && !cpu_online(2) && !cpu_online(3)) {
+		printk(KERN_INFO "pm-hotplug: disable cpu auto-hotplug with screen-off\n");
+		goto off_hotplug;
+	}
+
+	/* simone201 multi-core support */
+	if (!hotplug_on) {
+		
+		for(i = cores_on; i < NR_OF_CORES; i++) {
+			if(cpu_online(i) == CPU_ON)
+				cpu_down(i);
+		}
+		
+		for(i = 1; i < cores_on; i++) {
+			if (cpu_online(i) == CPU_OFF) {
+				cpu_up(i);
+			}
+		}
+		
+		goto off_hotplug;
+	}
+
 	if (user_lock == 1)
 		goto no_hotplug;
 
@@ -299,7 +340,10 @@ static void hotplug_timer(struct work_struct *work)
 
 no_hotplug:
 
-	queue_delayed_work_on(0, hotplug_wq, &hotplug_work, hotpluging_rate);
+	if(standhotplug_enabled)
+		queue_delayed_work_on(0, hotplug_wq, &hotplug_work, hotpluging_rate);
+
+off_hotplug:
 
 	mutex_unlock(&hotplug_lock);
 }
@@ -349,15 +393,176 @@ static struct notifier_block hotplug_reboot_notifier = {
 	.notifier_call = hotplug_reboot_notifier_call,
 };
 
+/****************************************
+ * DEVICE ATTRIBUTES FUNCTION by simone201/tegrak
+****************************************/
+#define declare_show(filename) \
+  static ssize_t show_##filename(struct device *dev, struct device_attribute *attr, char *buf)
+
+#define declare_store(filename) \
+  static ssize_t store_##filename(\
+    struct device *dev, struct device_attribute *attr, const char *buf, size_t size)
+    
+/****************************************
+ * multi_core attributes function by simone201/tegrak
+ ****************************************/
+declare_show(version) {
+  return sprintf(buf, "%u\n", MULTI_CORE_VERSION);
+}
+
+declare_show(author) {
+  return sprintf(buf, "simone201-tegrak\n");
+}
+
+declare_show(hotplug_on) {
+  return sprintf(buf, "%s\n", (hotplug_on) ? ("on") : ("off"));
+}
+
+declare_store(hotplug_on) {  
+  mutex_lock(&hotplug_lock);
+  
+  int i = 0;
+  
+  if (user_lock) {
+	cores_on = NR_OF_CORES;
+    goto finish;
+  }
+  
+  if (!hotplug_on && strcmp(buf, "on\n") == 0) {
+    hotplug_on = 1;
+    // restart worker thread.
+    hotpluging_rate = CHECK_DELAY_ON;
+    queue_delayed_work_on(0, hotplug_wq, &hotplug_work, hotpluging_rate);
+    printk("multi_core: hotplug is on!\n");
+  }
+  else if (hotplug_on && strcmp(buf, "off\n") == 0) {
+    hotplug_on = 0;
+    cores_on = NR_OF_CORES;
+    for(i = 1; i < NR_OF_CORES; i++) {
+		if (cpu_online(i) == 0) {
+			cpu_up(i);
+		}
+	}
+    printk("multi_core: hotplug is off!\n");
+  }
+  
+finish:
+  mutex_unlock(&hotplug_lock);
+  return size;
+}
+
+declare_show(cores_on) {
+  return sprintf(buf, "%d\n", cores_on);
+}
+
+declare_store(cores_on) {
+  mutex_lock(&hotplug_lock);
+  
+  unsigned int ret = -EINVAL;
+  int temp;
+  int i = 0;
+  
+  if (hotplug_on || user_lock) {
+	cores_on = NR_OF_CORES;
+    goto finish;
+  }
+  
+  ret = sscanf(buf,"%d", &temp);
+  if(ret != 1)
+	cores_on = NR_OF_CORES;
+  else
+	cores_on = temp;
+  
+  if((cores_on <= 0) || (cores_on > NR_OF_CORES)) {
+	printk("multi_core: nr of cores not valid! Reset to max: %d\n", NR_OF_CORES);
+	cores_on = NR_OF_CORES;
+  }
+  
+  printk("multi_core: nr of cores enabled: %d\n",cores_on);
+  
+  if(!hotplug_on) {
+	for(i = cores_on; i < NR_OF_CORES; i++) {
+		if (cpu_online(i) == CPU_ON) {
+			cpu_down(i);
+		}
+	}
+	
+	for(i = 1; i < cores_on; i++) {
+		if (cpu_online(i) == CPU_OFF) {
+			cpu_up(i);
+		}
+	}
+  }
+  
+finish:
+  mutex_unlock(&hotplug_lock);
+  return size;
+}
+
+/****************************************
+ * DEVICE ATTRIBUTE by simone201
+ ****************************************/
+#define declare_attr_rw(filename, perm) \
+  static DEVICE_ATTR(filename, perm, show_##filename, store_##filename)
+#define declare_attr_ro(filename, perm) \
+  static DEVICE_ATTR(filename, perm, show_##filename, NULL)
+#define declare_attr_wo(filename, perm) \
+  static DEVICE_ATTR(filename, perm, NULL, store_##filename)
+  
+declare_attr_ro(version, 0444);
+declare_attr_ro(author, 0444);
+declare_attr_rw(hotplug_on, 0666);
+declare_attr_rw(cores_on, 0666);
+
+static struct attribute *multi_core_attributes[] = {
+  &dev_attr_hotplug_on.attr, 
+  &dev_attr_cores_on.attr,
+  &dev_attr_version.attr,
+  &dev_attr_author.attr,
+  NULL
+};
+
+static struct attribute_group multi_core_group = {
+    .attrs  = multi_core_attributes,
+};
+
+static struct miscdevice multi_core_device = {
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = "multi_core",
+};
+
+static void hotplug_early_suspend(struct early_suspend *handler)
+{
+	mutex_lock(&hotplug_lock);
+	screen_off = true;
+	mutex_unlock(&hotplug_lock);
+}
+
+static void hotplug_late_resume(struct early_suspend *handler)
+{
+	printk(KERN_INFO "pm-hotplug: enable cpu auto-hotplug\n");
+
+	mutex_lock(&hotplug_lock);
+	screen_off = false;
+	queue_delayed_work_on(0, hotplug_wq, &hotplug_work, hotpluging_rate);
+	mutex_unlock(&hotplug_lock);
+}
+
+static struct early_suspend hotplug_early_suspend_notifier = {
+	.suspend = hotplug_early_suspend,
+	.resume = hotplug_late_resume,
+	.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN,
+};
+
 static int __init exynos4_pm_hotplug_init(void)
 {
 	unsigned int i;
 	unsigned int freq;
 	unsigned int freq_max = 0;
 	struct cpufreq_frequency_table *table;
+	int ret;
 
 	printk(KERN_INFO "EXYNOS4 PM-hotplug init function\n");
-	//hotplug_wq = create_workqueue("dynamic hotplug");
 	hotplug_wq = alloc_workqueue("dynamic hotplug", 0, 0);
 	if (!hotplug_wq) {
 		printk(KERN_ERR "Creation of hotplug work failed\n");
@@ -386,6 +591,20 @@ static int __init exynos4_pm_hotplug_init(void)
 #endif
 	register_pm_notifier(&exynos4_pm_hotplug_notifier);
 	register_reboot_notifier(&hotplug_reboot_notifier);
+	register_early_suspend(&hotplug_early_suspend_notifier);
+
+	/* simone201 multi-core support */
+	ret = misc_register(&multi_core_device);
+	if (ret) {
+		printk(KERN_ERR "failed at(%d)\n", __LINE__);
+		return ret;
+	}
+	
+	ret = sysfs_create_group(&multi_core_device.this_device->kobj, &multi_core_group);
+	if (ret) {
+		printk(KERN_ERR "failed at(%d)\n", __LINE__);
+		return ret;
+	}
 
 	return 0;
 }
@@ -395,6 +614,55 @@ late_initcall(exynos4_pm_hotplug_init);
 static struct platform_device exynos4_pm_hotplug_device = {
 	.name = "exynos4-dynamic-cpu-hotplug",
 	.id = -1,
+};
+
+/* enable-disable stand hotplug with hotplug-aware govs - thx to gm */
+static int standhotplug_cpufreq_policy_notifier_call(struct notifier_block *this,
+												unsigned long code, void *data)
+{
+	struct cpufreq_policy *policy = data;
+
+	switch (code) {
+		case CPUFREQ_ADJUST:
+			if (
+				(!strnicmp(policy->governor->name, "pegasusq", CPUFREQ_NAME_LEN)) ||
+				(!strnicmp(policy->governor->name, "hotplug", CPUFREQ_NAME_LEN))
+				)
+			{
+				
+				if(standhotplug_enabled)
+				{
+					DBG_PRINT("Stand-hotplug is disabled: governor=%s\n",
+								policy->governor->name);
+					mutex_lock(&hotplug_lock);
+					standhotplug_enabled = false;
+					mutex_unlock(&hotplug_lock);
+				}
+			}
+			else
+			{
+				if(!standhotplug_enabled)
+				{
+					DBG_PRINT("Stand-hotplug is enabled: governor=%s\n",
+								policy->governor->name);
+					mutex_lock(&hotplug_lock);
+					standhotplug_enabled = true;
+					queue_delayed_work_on(0, hotplug_wq, &hotplug_work, hotpluging_rate);
+					mutex_unlock(&hotplug_lock);
+				}
+			}
+			break;
+		case CPUFREQ_INCOMPATIBLE:
+		case CPUFREQ_NOTIFY:
+		default:
+			break;
+	}
+
+	return NOTIFY_DONE;
+}
+
+static struct notifier_block standhotplug_cpufreq_policy_notifier = {
+	.notifier_call = standhotplug_cpufreq_policy_notifier_call,
 };
 
 static int __init exynos4_pm_hotplug_device_init(void)
@@ -409,6 +677,9 @@ static int __init exynos4_pm_hotplug_device_init(void)
 	}
 
 	printk(KERN_INFO "exynos4_pm_hotplug_device_init: %d\n", ret);
+
+	cpufreq_register_notifier(&standhotplug_cpufreq_policy_notifier,
+								CPUFREQ_POLICY_NOTIFIER);
 
 	return ret;
 }
