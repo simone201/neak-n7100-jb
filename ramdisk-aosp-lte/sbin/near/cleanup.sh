f51568165fb133c1ee544bcbe0615fbecb0350ea
Simone Renzo <simone201@gmail.com>
2013-01-19 18:04:55 +0100

    Adding CM10.1 ramdisks for N7105

---
+++ ramdisk-aosp-lte/sbin/near/cleanup.sh
@@ -0,0 +1,27 @@
+#!/sbin/busybox sh
+# Doing some cleanup
+# by Simone201
+
+/sbin/busybox mount -o remount,rw /system
+
+if [ -e /system/etc/init.d/S98bolt_siyah ]; then
+	/sbin/busybox rm /system/etc/init.d/S98bolt_siyah
+fi;
+
+if [ -e /system/etc/init.d/s78enable_touchscreen_1 ]; then
+	/sbin/busybox rm /system/etc/init.d/s78enable_touchscreen_1
+fi;
+
+if [ -e /system/etc/init.d/S98CFS ]; then
+	/sbin/busybox rm /system/etc/init.d/S98CFS
+fi;
+
+# Creating init.d folder if it doesn't exist
+if [ ! -d /system/etc/init.d ]; then
+	/sbin/busybox mkdir /system/etc/init.d
+fi;
+
+# Setting the right script permissions
+/sbin/busybox chmod 755 /system/etc/init.d/*
+
+/sbin/busybox mount -o remount,ro /system
---
+++ ramdisk-aosp-lte/sbin/post-init.sh
@@ -0,0 +1,148 @@
+#!/sbin/busybox sh
+# thanks to hardcore and nexxx
+# thanks to knzo, gokhanmoral, pikachu01
+# modded by simone201 for NEAK Kernel
+
+MMC=`ls -d /sys/block/mmc*`;
+
+/sbin/busybox cp /data/user.log /data/user.log.bak
+/sbin/busybox rm /data/user.log
+exec >>/data/user.log
+exec 2>&1
+
+echo $(date) START of post-init.sh
+
+##### Early-init phase #####
+
+# IPv6 privacy tweak
+  echo "2" > /proc/sys/net/ipv6/conf/all/use_tempaddr
+
+# Remount all partitions with noatime
+  for k in $(/sbin/busybox mount | /sbin/busybox grep relatime | /sbin/busybox cut -d " " -f3)
+  do
+        sync
+        /sbin/busybox mount -o remount,noatime $k
+  done
+
+# Remount ext4 partitions with optimizations
+  for k in $(/sbin/busybox mount | /sbin/busybox grep ext4 | /sbin/busybox cut -d " " -f3)
+  do
+        sync
+        /sbin/busybox mount -o remount,commit=15 $k
+  done
+  
+# Miscellaneous tweaks
+  echo "12288" > /proc/sys/vm/min_free_kbytes
+  echo "1500" > /proc/sys/vm/dirty_writeback_centisecs
+  echo "200" > /proc/sys/vm/dirty_expire_centisecs
+  echo "0" > /proc/sys/vm/swappiness
+
+# Pegasus CPU hotplug tweaks - thx to hardcore
+  echo "500000" > /sys/devices/system/cpu/cpufreq/pegasusq/hotplug_freq_1_1
+  echo "800000" > /sys/devices/system/cpu/cpufreq/pegasusq/hotplug_freq_2_1
+  echo "800000" > /sys/devices/system/cpu/cpufreq/pegasusq/hotplug_freq_3_1
+  echo "400000" > /sys/devices/system/cpu/cpufreq/pegasusq/hotplug_freq_2_0
+  echo "600000" > /sys/devices/system/cpu/cpufreq/pegasusq/hotplug_freq_3_0
+  echo "600000" > /sys/devices/system/cpu/cpufreq/pegasusq/hotplug_freq_4_0
+
+  echo "100" > /sys/devices/system/cpu/cpufreq/pegasusq/hotplug_rq_1_1
+  echo "100" > /sys/devices/system/cpu/cpufreq/pegasusq/hotplug_rq_2_0
+  echo "200" > /sys/devices/system/cpu/cpufreq/pegasusq/hotplug_rq_2_1
+  echo "200" > /sys/devices/system/cpu/cpufreq/pegasusq/hotplug_rq_3_0
+  echo "300" > /sys/devices/system/cpu/cpufreq/pegasusq/hotplug_rq_3_1
+  echo "300" > /sys/devices/system/cpu/cpufreq/pegasusq/hotplug_rq_4_0
+
+# SD cards (mmcblk) read ahead tweaks
+  echo "512" > /sys/devices/virtual/bdi/179:0/read_ahead_kb
+  echo "512" > /sys/devices/virtual/bdi/179:16/read_ahead_kb
+  echo "512" > /sys/devices/virtual/bdi/default/read_ahead_kb
+  echo "256" > /sys/block/mmcblk0/bdi/read_ahead_kb
+  echo "256" > /sys/block/mmcblk1/bdi/read_ahead_kb
+
+# TCP tweaks
+  echo "0" > /proc/sys/net/ipv4/tcp_timestamps;
+  echo "1" > /proc/sys/net/ipv4/tcp_tw_reuse;
+  echo "1" > /proc/sys/net/ipv4/tcp_sack;
+  echo "1" > /proc/sys/net/ipv4/tcp_tw_recycle;
+  echo "1" > /proc/sys/net/ipv4/tcp_window_scaling;
+  echo "2" > /proc/sys/net/ipv4/tcp_syn_retries;
+  echo "2" > /proc/sys/net/ipv4/tcp_synack_retries;
+  echo "10" > /proc/sys/net/ipv4/tcp_fin_timeout;
+  echo "404480" > /proc/sys/net/core/wmem_max;
+  echo "404480" > /proc/sys/net/core/rmem_max;
+  echo "256960" > /proc/sys/net/core/rmem_default;
+  echo "256960" > /proc/sys/net/core/wmem_default;
+  echo "4096 16384 404480" > /proc/sys/net/ipv4/tcp_wmem;
+  echo "4096 87380 404480" > /proc/sys/net/ipv4/tcp_rmem;
+
+# New scheduler tweaks + readahead tweaks (thx to Pikachu01)
+for i in $MMC;
+do
+	if [ -e $i/queue/rotational ]; 
+	then
+		echo "0" > $i/queue/rotational; 
+	fi;
+	if [ -e $i/queue/nr_requests ];
+	then
+		echo "8192" > $i/queue/nr_requests;
+	fi;
+done;
+
+# Misc Kernel Tweaks
+/sbin/busybox sysctl -w vm.vfs_cache_pressure=70
+echo "8" > /proc/sys/vm/page-cluster;
+echo "64000" > /proc/sys/kernel/msgmni;
+echo "64000" > /proc/sys/kernel/msgmax;
+echo "10" > /proc/sys/fs/lease-break-time;
+/sbin/busybox sysctl -w kernel.sem="500 512000 100 2048";
+/sbin/busybox sysctl -w kernel.shmmax=268435456;
+
+# Turn off debugging for certain modules
+  echo "0" > /sys/module/wakelock/parameters/debug_mask
+  echo "0" > /sys/module/userwakelock/parameters/debug_mask
+  echo "0" > /sys/module/earlysuspend/parameters/debug_mask
+  echo "0" > /sys/module/alarm/parameters/debug_mask
+  echo "0" > /sys/module/alarm_dev/parameters/debug_mask
+  echo "0" > /sys/module/binder/parameters/debug_mask
+  echo "0" > /sys/module/lowmemorykiller/parameters/debug_level
+
+# Doing some cleanup before init.d support & neak options
+    /sbin/busybox sh /sbin/near/cleanup.sh
+	
+# NEAK Options
+	/sbin/busybox sh /sbin/near/neak-options.sh
+
+# Set VR as default scheduler - workaround
+echo "vr" > /sys/block/mmcblk0/queue/scheduler
+echo "vr" > /sys/block/mmcblk1/queue/scheduler
+
+echo $(date) PRE-INIT DONE of post-init.sh
+
+##### Post-init phase #####
+
+sleep 10
+
+# init.d support
+echo $(date) USER INIT START from /system/etc/init.d
+if cd /system/etc/init.d >/dev/null 2>&1 ; then
+    for file in * ; do
+        if ! ls "$file" >/dev/null 2>&1 ; then continue ; fi
+        echo "START '$file'"
+        /system/bin/sh "$file"
+        echo "EXIT '$file' ($?)"
+    done
+fi
+echo $(date) USER INIT DONE from /system/etc/init.d
+
+echo $(date) USER INIT START from /data/init.d
+if cd /data/init.d >/dev/null 2>&1 ; then
+    for file in * ; do
+        if ! ls "$file" >/dev/null 2>&1 ; then continue ; fi
+        echo "START '$file'"
+        /system/bin/sh "$file"
+        echo "EXIT '$file' ($?)"
+    done
+fi
+echo $(date) USER INIT DONE from /data/init.d
+
+echo $(date) END of post-init.sh
