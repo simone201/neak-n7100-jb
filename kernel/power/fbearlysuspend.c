102acf1ab7d46875108e9a0028745102a4a77d91
Simone Renzo <simone201@gmail.com>
2013-03-28 12:02:24 +0100

    fbearlysuspend: reduced msleep to 100ms

--- kernel/power/fbearlysuspend.c
+++ kernel/power/fbearlysuspend.c
@@ -38,7 +38,7 @@ static void stop_drawing_early_suspend(struct early_suspend *h)
 	unsigned long irq_flags;
 
 #ifndef CONFIG_AOSP_ROM_SUPPORT
-	msleep(400);
+	msleep(100);
 #endif
 
 	spin_lock_irqsave(&fb_state_lock, irq_flags);
