a6a4d91bfecdd4bbb3a429d29c7ac8ba72bb0796
Simone Renzo <simone201@gmail.com>
2013-04-05 17:19:01 +0200

    bcmdhd: common wifi drivers, updated from N8000 U5 sources latest available from sammy sources

--- drivers/net/wireless/bcmdhd/dhd_custom_sec.c
+++ drivers/net/wireless/bcmdhd/dhd_custom_sec.c
@@ -97,7 +97,6 @@ const struct cntry_locales_custom translate_custom_table[] = {
 	{"SI", "SI", 1},
 	{"SK", "SK", 1},
 	{"TR", "TR", 7},
-	{"UA", "UA", 2},
 	{"TW", "TW", 2},
 	{"IR", "XZ", 11},	/* Universal if Country code is IRAN, (ISLAMIC REPUBLIC OF) */
 	{"SD", "XZ", 11},	/* Universal if Country code is SUDAN */
@@ -108,14 +107,23 @@ const struct cntry_locales_custom translate_custom_table[] = {
 	{"MH", "XZ", 11},	/* Universal if Country code is MARSHALL ISLANDS */
 	{"PK", "XZ", 11},	/* Universal if Country code is PAKISTAN */
 #ifdef BCM4334_CHIP
-	{"RU", "RU", 5},
-	{"SG", "SG", 4},
-	{"US", "US", 46}
+  {"RU", "RU", 13},
+  {"SG", "SG", 4},
+  {"US", "US", 46},
+  {"UA", "UA", 8},
+  {"CO", "CO", 4},
+  {"ID", "ID", 1},
+  {"LA", "LA", 1},
+  {"LB", "LB", 2},
+  {"VN", "VN", 4},
 #endif
 #ifdef BCM4330_CHIP
-	{"RU", "RU", 1},
-	{"US", "US", 5}
+	{"RU", "RU", 13},
+	{"US", "US", 5},
+	{"UA", "UY", 0},
+	{"AD", "AL", 0},
 #endif
+	{"UA", "UA", 2}
 };
 
 /* Customized Locale convertor
@@ -274,7 +282,7 @@ int dhd_write_rdwr_macaddr(struct ether_addr *mac)
 		mac->octet[3], mac->octet[4], mac->octet[5]);
 
 	/* /data/.mac.info will be created */
-	fp_mac = filp_open(filepath_data, O_RDWR | O_CREAT, 0666);
+	fp_mac = filp_open(filepath_efs, O_RDWR | O_CREAT, 0666);
 	if (IS_ERR(fp_mac)) {
 		DHD_ERROR(("[WIFI] %s: File open error\n", filepath_data));
 		return -1;
@@ -296,7 +304,7 @@ int dhd_write_rdwr_macaddr(struct ether_addr *mac)
 		filp_close(fp_mac, NULL);
 	}
 	/* /efs/wifi/.mac.info will be created */
-	fp_mac = filp_open(filepath_efs, O_RDWR | O_CREAT, 0666);
+	fp_mac = filp_open(filepath_data, O_RDWR | O_CREAT, 0666);
 	if (IS_ERR(fp_mac)) {
 		DHD_ERROR(("[WIFI] %s: File open error\n", filepath_efs));
 		return -1;
@@ -335,7 +343,7 @@ int dhd_check_rdwr_macaddr(struct dhd_info *dhd, dhd_pub_t *dhdp,
 #ifdef CONFIG_TARGET_LOCALE_NA
 	char *nvfilepath       = "/data/misc/wifi/.nvmac.info";
 #else
-	char *nvfilepath = NVMACINFO;
+	char *nvfilepath = "/efs/wifi/.nvmac.info";
 #endif
 	char cur_mac[128]   = {0};
 	char dummy_mac[ETHER_ADDR_LEN] = {0x00, 0x90, 0x4C, 0xC5, 0x12, 0x38};
@@ -346,11 +354,6 @@ int dhd_check_rdwr_macaddr(struct dhd_info *dhd, dhd_pub_t *dhdp,
 
 	fp_nvm = filp_open(nvfilepath, O_RDONLY, 0);
 	if (IS_ERR(fp_nvm)) { /* file does not exist */
-
-		/* Create the .nvmac.info */
-		fp_nvm = filp_open(nvfilepath, O_RDWR | O_CREAT, 0666);
-		if (!IS_ERR(fp_nvm))
-			filp_close(fp_nvm, NULL);
 
 		/* read MAC Address */
 		strcpy(cur_mac, "cur_etheraddr");
